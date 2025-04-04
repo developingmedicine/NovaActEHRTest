ec5de998627ae41264c63ea4e35825f2943c5974
Jae Won Joh <developingmedicine@gmail.com>
2025-04-03 22:45:50 -0700

    Proof of concept using OpenEMR's Demo page, extracting medical problems from a patient

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from nova_act import NovaAct
+from pydantic import BaseModel
 
-with NovaAct(starting_page="https://www.amazon.com") as nova:
-    nova.act("search for a coffee maker")
-    nova.act("select the first result")
-    nova.act("scroll down or up until you see 'add to cart' and then click 'add to cart'")
+class MedicalProblem(BaseModel):
+    Problem: str
+
+class MedicalProblemList(BaseModel):
+    ProblemList: list[MedicalProblem]
+
+with NovaAct(starting_page="https://demo.openemr.io/openemr/interface/login/login.php?site=default") as nova:
+    nova.act("Log in to the OpenEMR demo site, using the username 'physician' and password 'physician'")
+    nova.act("Click the 'Finder' buttom at the top of the page")
+    nova.act("In the 'Search by Name' box, type in 'Belford'")
+    nova.act("Select the first patient in the list")
+
+    result = nova.act("Return the currently visible list of medical problems", schema=MedicalProblemList.model_json_schema())
+
+    if not result.matches_schema:
+        print(f"Invalid JSON {result=}")
+
+    medical_problem_list = MedicalProblemList.model_validate(result.parsed_response)
+
+    print(f"Clinical Reminder List: {medical_problem_list}")
